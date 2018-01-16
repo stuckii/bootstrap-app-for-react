623e6a932bbf971651b3b335d082ada5b20a8117
t1t1 <cha@hanmail2.net>
2018-01-16 19:50:09 +0900

    #3 state init

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,13 +3,18 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class Todos extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [1, 2, 3]
+        };
+    }
     render() {
         return (
             <div className="todos">
                 <TodoInput />
                 <TodoList
-                    todos={[1, 2, 3]}
+                    todos={this.state.todos}
                 />
             </div>
         );
