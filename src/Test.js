a1d130e3a83aa993473a67979bb370e28cc0c34d
t1t1 <cha@hanmail2.net>
2018-01-16 12:57:23 +0900

    #1 Add ‘Test’ component

---
+++ src/Test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Test.css';
+
+class Test extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                Test component area
+            </div>
+            
+        );
+    }
+}
+
+export default Test;
