f3c649aa796ff752d99c150168e285e75c0e97d9
t1t1 <cha@hanmail2.net>
2018-01-19 13:02:18 +0900

    #2 Split components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+
 import logo from './logo.svg';
+
+import Home from './components/Home'
 import Todos from './components/Todos'
+import About from './components/About'
+import Topics from './components/Topics'
 
 class App extends Component {
   render() {
@@ -32,49 +37,3 @@ class App extends Component {
 }
 
 export default App;
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
